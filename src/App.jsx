e21f813577bf9d68aa5c05233c7dd5e286611dd3
Kenzhie16 <kelaco16@gmail.com>
2023-02-07 16:57:56 +0800

    updating files

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import './index.css';
+import '/css/styles.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronRight, faChevronLeft, faCircle, faCheckCircle, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faChevronRight, faChevronLeft, faCircle, faCheckCircle, faPlus, faTrashCan } from '@fortawesome/free-solid-svg-icons';
 
 const App = () => {
-	// HINT: each "item" in our list names a name,
-	// a boolean to tell if its been completed, and a quantity
-	const [items, setItems] = useState([
-	]);
-
+	
+	const [items, setItems] = useState([]);
+  const [showAlert, setShowAlert] = useState(false);
 	const [inputValue, setInputValue] = useState('');
 	const [totalItemCount, setTotalItemCount] = useState(0);
 
 	const handleAddButtonClick = () => {
-		const newItem = {
-			itemName: inputValue,
-			quantity: 0,
-			isSelected: false,
-		};
+
+    if (inputValue == "") {
+      setShowAlert(true);
+  } else {
+      const newItem = {
+          id: Math.floor(Math.random() * 1000),
+          itemName: inputValue,
+          quantity: 1,
+          isSelected: false,
+      };
 
 		const newItems = [...items, newItem];
 
 		setItems(newItems);
 		setInputValue('');
-		calculateTotal();
-	};
+		setTotalItemCount(totalItemCount + 1);
+	}
+};
+
+  const closeAlert = () => {
+    setShowAlert(false);
+}
 
 	const handleQuantityIncrease = (index) => {
 		const newItems = [...items];
@@ -38,11 +47,23 @@ const App = () => {
 	const handleQuantityDecrease = (index) => {
 		const newItems = [...items];
 
-		newItems[index].quantity--;
+		if(newItems[index].quantity <= 1) {
+      newItems[index].quantity = 1;
+  } else {
+    newItems[index].quantity--;
+  }
 
 		setItems(newItems);
 		calculateTotal();
 	};
+
+  const deleteItem = (index) => {
+    const newItems = [...items];
+    const deletedItem = newItems.splice(index,1)[0];
+    setItems(newItems);
+    setTotalItemCount((prevCount) => prevCount - deletedItem.quantity);
+  };
+
 
 	const toggleComplete = (index) => {
 		const newItems = [...items];
@@ -61,10 +82,25 @@ const App = () => {
 	};
 
 	return (
+
+    <div className='App'>
+
+
+
 		<div className='app-background'>
+
+    {showAlert && (
+    <div type="button" className="btn-close" data-bs-dismiss="alert" data-bs-target="#my-alert" aria-label="Close">
+        <button onClick={() => closeAlert()} type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        <strong>Oh snap!</strong> Write something and try adding again.
+    </div>
+  )}
+
 			<div className='main-container'>
+        <h2>WishList</h2>
 				<div className='add-item-box'>
 					<input value={inputValue} onChange={(event) => setInputValue(event.target.value)} className='add-item-input' placeholder='Add an item...' />
+          
 					<FontAwesomeIcon icon={faPlus} onClick={() => handleAddButtonClick()} />
 				</div>
 				<div className='item-list'>
@@ -91,13 +127,18 @@ const App = () => {
 								<button>
 									<FontAwesomeIcon icon={faChevronRight} onClick={() => handleQuantityIncrease(index)} />
 								</button>
+                <button>
+									<FontAwesomeIcon icon={faTrashCan} onClick={() => deleteItem(index)} />
+								</button>
 							</div>
 						</div>
 					))}
 				</div>
+        
 				<div className='total'>Total: {totalItemCount}</div>
 			</div>
 		</div>
+  </div>
 	);
 };
 
